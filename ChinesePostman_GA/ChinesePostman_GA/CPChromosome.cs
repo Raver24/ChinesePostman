e37f9950516a264c290c2d87e5a6f24b25896420
dmnmlk <dmnmlk@gmail.com>
2017-11-22 22:05:27 +0100

    skończenie generowania poprawnego chromosomu: sprawdza teraz czy skończył tam gdzie zaczął i czy wszystkie ścieżki zostały przebyte

--- ChinesePostman_GA/ChinesePostman_GA/CPChromosome.cs
+++ ChinesePostman_GA/ChinesePostman_GA/CPChromosome.cs
@@ -3,6 +3,7 @@ using GeneticSharp.Domain.Randomizations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,6 +13,7 @@ namespace ChinesePostman_GA
     {
         #region Fields
         private int m_numberOfRoads;
+        private int startingPoint;
         #endregion
 
         #region Constructors
@@ -25,25 +27,66 @@ namespace ChinesePostman_GA
 
             m_numberOfRoads = numberOfRoads;
             int[] roadsIndexes = new int[numberOfRoads]; //list to hold indexes of roads in order
+
             Random random = new Random(); //object to random mechanism
             roadsIndexes[0] = random.Next(roads.Count); //Random search for first road
-            //Console.WriteLine(roads[roadsIndexes[0]].index);
+
+            startingPoint = roads[roadsIndexes[0]].cityFrom; // point from where we start
+
+            Console.WriteLine("pierwsza sciezka");
+            Console.WriteLine(roads[roadsIndexes[0]].index);
+
             for (int i = 1; i < m_numberOfRoads; i++)
             {
                 List<Road> filtered = roads.Where(e => e.cityFrom.Equals(roads[roadsIndexes[i - 1]].cityTo)).ToList(); //new list with filtered roads, that have cityFrom set as same as previous element cityTo
+
+
+                if (filtered.Count.Equals(0))
+                {
+                    break;
+                }
                 else
                 {
                     int selectedFromFilter = random.Next(filtered.Count); //get random road from filtered ones
                     roadsIndexes[i] = roads.IndexOf(filtered[selectedFromFilter]);//set this road as next
-                    //Console.WriteLine(roads[roadsIndexes[i]].index);
+
+                    roads[roadsIndexes[i]].isTravelled = true; // set that this road is travelled
+                    Road returnRoad = roads.Find(e => e.index.Equals(roads[roadsIndexes[i]].cityTo.ToString() + roads[roadsIndexes[i]].cityFrom.ToString())); // getting return road object
+                    if (returnRoad != null) // resisting exceptions when return road is not present in file
+                    {
+                        returnRoad.isTravelled = true; // set that the reverce of the road is travelled
+                    }
+                    
+                    Console.WriteLine(roads[roadsIndexes[i]].index);
+
+                    if (roads[roadsIndexes[i]].cityTo == startingPoint && everyRoadIsTraveled()) // checking if the current end point is exact as starting point and if all roads were travelled at least once
+                    {
+                        Console.WriteLine("koniec");
+                        Console.ReadLine();
+                        break; // we have generated valid chromosome, end
+                    }
+
                 }
                 
             }
-
+            Console.ReadLine();
             for (int i = 0; i < numberOfRoads; i++)
             {
                 ReplaceGene(i, new Gene(roadsIndexes[i]));
             }
+        }
+
+        public bool everyRoadIsTraveled()
+        {
+            List<Road> roads = Program.roads;
+            foreach (var road in roads)
+            {
+                if(road.isTravelled == false)
+                {
+                    return false;
+                }
+            }
+            return true;
         }
         #endregion
 
